b035568988624fb6bda251737cf750ae365f168b
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2025-01-15 09:31:37 +0200

    wip manpage args

--- mk/common.mk
+++ mk/common.mk
@@ -9,8 +9,17 @@ ifeq ($(OS),Windows_NT)
 EXE_EXT:=.exe
 endif
 
+ifeq ("x$(MSYSTEM_PREFIX)","x")
+INSTALL_PREFIX:=/usr/local
+else
+INSTALL_PREFIX:=$(MYSTEM_PREFIX)
+endif
+
 SRC:=$(NAME).c $(EXTRA_SRC)
 EXE:=$(NAME)$(EXE_EXT)
+
+MAN_SRC:=$(NAME).1
+MAN:=$(addsuffix .gz, $(MAN_SRC))
 
 all: $(EXE)
 
@@ -21,5 +30,10 @@ $(EXE): $(SRC)
 	cc $(CFLAGS) -O2 $(SRC) -o $(NAME)
 	strip $(EXE)
 
-install: $(EXE)
-	cp -f $(EXE) /usr/local/bin
+%.1.gz: %.1
+	gzip -k9 $<
+
+install: $(EXE) $(MAN)
+	install -d "$(INSTALL_PREFIX)/bin" "$(INSTALL_PREFIX)/share/man/man1"
+	install $(EXE) "$(INSTALL_PREFIX)/bin"
+	install $(MAN) "$(INSTALL_PREFIX)/share/man/man1"
